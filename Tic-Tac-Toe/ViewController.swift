a0cd1b2174dc670ad336fd192320f5dd3295f67d
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-07-28 20:11:48 -0500

    Added draw option

--- Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/ViewController.swift
@@ -16,11 +16,13 @@ class ViewController: UIViewController {
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     var gameActive = true
     var player = 1
+    var counter = 0
     var winnerCombination = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     @IBAction func playAgainButton(sender: AnyObject) {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         gameActive = true
         player = 1
+        counter = 0
         var button = UIButton()
         for var i = 0; i < 9; i++ {
             button = view.viewWithTag(i) as! UIButton
@@ -35,11 +37,11 @@ class ViewController: UIViewController {
         var image = UIImage()
         if gameState[sender.tag] == 0 && gameActive == true{
             if player == 1 {
-                image = UIImage(named: "nought.png")!
+                image = UIImage(named: "juan.jpg")!
                 gameState[sender.tag] = player
                 player = 2
             }else{
-                image = UIImage(named: "cross.png")!
+                image = UIImage(named: "sara.jpg")!
                 gameState[sender.tag] = player
                 player = 1
             }
@@ -47,9 +49,9 @@ class ViewController: UIViewController {
             for combination in winnerCombination{
                 if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
                     if gameState[combination[1]] == 1{
-                        resultMessage.text = "Noughts has won"
-                    }else{
-                        resultMessage.text = "Crosses has won"
+                        resultMessage.text = "Juan has won"
+                    }else if gameState[combination[1]] == 2{
+                        resultMessage.text = "Sara has won"
                     }
                     resultMessage.hidden = false
                     playAgain.hidden = false
@@ -60,6 +62,18 @@ class ViewController: UIViewController {
                     
                     gameActive = false
                 }
+                
+                counter++;
+                
+            }
+            if counter >= 72 {
+                    resultMessage.text = "It's a draw"
+                    resultMessage.hidden = false
+                    playAgain.hidden = false
+                    UIView.animateWithDuration(0.5, animations: { () -> Void in
+                        self.resultMessage.center = CGPointMake(self.resultMessage.center.x + 400, self.resultMessage.center.y)
+                        self.playAgain.center = CGPointMake(self.playAgain.center.x + 400, self.playAgain.center.y)
+                    })
             }
             sender.setImage(image, forState: .Normal)
         }
